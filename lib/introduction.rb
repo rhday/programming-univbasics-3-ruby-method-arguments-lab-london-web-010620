ab330d527fc00cca1edcbf629ff9bcf87e588358
Ruari Day <ruariday@gmail.com>
2019-11-20 18:20:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 name = "Dan"
-language = "Ruaby"
+language = "Ember.js"
 def introduction_with_language(name, language)
   puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
